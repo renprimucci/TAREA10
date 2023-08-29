773d823c38ad2c67064c5c02e0947b71fb8dc240
renprimucci <140030571+renprimucci@users.noreply.github.com>
2023-08-28 23:28:18 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var nombre = "Renzo"
+var apellido = "Primucci"
+
+console.log (nombre + " " + apellido)
+
+
+
+var numero1 = 5;
+var numero2 = 8;
+var suma = numero1 + numero2
+
+console.log(suma)
+
+
+
+var numero3 = suma + 8;
+
+console.log (numero3)
